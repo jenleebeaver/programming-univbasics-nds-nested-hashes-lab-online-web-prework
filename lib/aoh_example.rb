adb9916adb1588c7733246c0ff085b180cc71ca6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-06 07:33:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,8 +35,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] = new_value
-  return aoh
+  literal_aoh[row][key] = new_value
+  return literal_aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
